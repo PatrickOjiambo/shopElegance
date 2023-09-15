0e601f52351c5b3d884357c4981347c0c6edd903
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-09-15 14:49:48 +0300

    Products based on category

--- models/ProductModels/getProduct.js
+++ models/ProductModels/getProduct.js
@@ -1,6 +1,22 @@
 import pool from '../createdb.js';
+//Get product based on the category name from the controller
 
-export function getProducts () {
-  const getProductQuery = 'SELECT';
+//TODO - IMPLEMENT PAGINATION OF THE RETRIEVED RESULTS
+
+const poolPromise = pool.promise()
+/**
+ * 
+ * @param {string} category_name - Name of the category
+ */
+export async function getProducts (category_name) {
+  
+try{
+  const getProductQuery = 'SELECT * FROM Categories WHERE name = ?';
+  const categoryResults = await poolPromise.query(getProductQuery, [category_name])
+  req.status(200).json({category_name: categoryResults})
+}
+catch(error){
+resizeBy.status(500).json({'Server error': error})
+}
 }
 module.exports = { getProducts };
