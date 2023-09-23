02e7f0a17dad7a00c1c3c685d4be474094f25f60
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-09-23 21:19:34 +0300

    Arguably done

--- controllers/Orders/createOrderController.js
+++ controllers/Orders/createOrderController.js
@@ -1,13 +1,19 @@
-import pool from "../createdb.js";
 import { createOrder } from "../../models/OrderModels/createOrder.js";
+import pool from '../../models/createdb.js'
+
+const poolPromise = pool.promise()
 //user_id from session storage or cookies. product_id from the product object.
 /**
  *
  * @param {*Request } req - Request should come with product_id and user_id
- * @param {*} res
+ * @param {Response} res - Response object
  */
 export async function createOrderControlller(req, res) {
   try {
+    const email = req.user.email
+    const userIdQuery = 'SELECT user_id FROM Users WHERE email = ?'
+    const userResult = poolPromise.query(userIdQuery, [email])
+    const user_id = userResult[0]
     const { status, total_amount, product_id, quantity, item_price } = req.body;
     const orderObject = { user_id, status, total_amount };
     const orderItemObjects = { product_id, quantity, item_price };
---
+++ controllers/Reviews/createReviewController.js
@@ -0,0 +1,21 @@
+import pool from "../../models/createdb.js";
+//Ensure the user_id and product_id are passed nicely to
+// the reviews
+import { createReview } from "../../models/ReviewModels/createReview.js";
+export async function createReviewController(req, res) {
+  try {
+    const email = req.user.email;
+    const userIdQuery = "SELECT user_id FROM Users WHERE email = ?";
+    const userResult = poolPromise.query(userIdQuery, [email]);
+    const user_id = userResult[0];
+    const { product_id, comment, rating } = req.body;
+    const createReviewResults = createReview(
+      user_id,
+      product_id,
+      comment,
+      rating
+    );
+  } catch (error) {
+    console.log("Error creating review", error);
+  }
+}
